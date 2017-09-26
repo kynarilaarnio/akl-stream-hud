05c02da5b6f57f45934099f2eca0c38044f60beb
Sauli Anto <sauli.anto@gmail.com>
2017-09-27 02:18:27 +0300

    Add 2 logo variant

--- public/hud.js
+++ public/hud.js
@@ -5,47 +5,43 @@ var bombLeftTime;
 var bombTime;
 var bombPlantedTime;
 var timer = document.getElementById("timer");
-sock.on("message", function(data){
-    //console.log(data.phase_countdowns)
-    if (data.phase_countdowns != null) {
-        if (data.phase_countdowns.phase == "bomb") {
-            bombTime = parseFloat(data.phase_countdowns.phase_ends_in);
-            if (bombStartTime == undefined) {
-                bombStartTime = parseFloat(data.phase_countdowns.phase_ends_in);
-                console.log("planted", bombStartTime);
-                var d = new Date();
-                bombPlantedTime = d.getTime();
-                console.log("planted time", bombPlantedTime);
-            }
-
-        } else if (data.phase_countdowns.phase == "over") {
-            bombStartTime = undefined;
-
-        } else if (data.phase_countdowns.phase == "freezetime") {
-            timer.style.width = "0%";
-
-        }
+sock.on("message", function(data) {
+  //console.log(data.phase_countdowns)
+  if (data.phase_countdowns != null) {
+    if (data.phase_countdowns.phase == "bomb") {
+      bombTime = parseFloat(data.phase_countdowns.phase_ends_in);
+      if (bombStartTime == undefined) {
+        bombStartTime = parseFloat(data.phase_countdowns.phase_ends_in);
+        console.log("planted", bombStartTime);
+        var d = new Date();
+        bombPlantedTime = d.getTime();
+        console.log("planted time", bombPlantedTime);
+      }
+    } else if (data.phase_countdowns.phase == "over") {
+      bombStartTime = undefined;
+    } else if (data.phase_countdowns.phase == "freezetime") {
+      timer.style.width = "0%";
     }
+  }
 });
 
-function createBombTimer(){
+function createBombTimer() {
+  setInterval(function() {
+    console.log("Left", bombLeftTime);
 
-    setInterval(function(){
-        console.log("Left", bombLeftTime);
-
-        if (bombLeftTime <= 5) {
-            timer.style.backgroundColor = "red";
-        } else if (bombLeftTime <= 10) {
-            timer.style.backgroundColor = "yellow";
-        } else {
-            timer.style.backgroundColor = "green";
-        }
-        var d = new Date();
-        var now = d.getTime();
-        bombLeftTime = bombStartTime - ((now - bombPlantedTime) / 1000);
-        timer.style.width = bombLeftTime / bombStartTime * 100  + "%";
-    }, 16);
+    if (bombLeftTime <= 5) {
+      timer.style.backgroundColor = "red";
+    } else if (bombLeftTime <= 10) {
+      timer.style.backgroundColor = "yellow";
+    } else {
+      timer.style.backgroundColor = "green";
+    }
+    var d = new Date();
+    var now = d.getTime();
+    bombLeftTime = bombStartTime - (now - bombPlantedTime) / 1000;
+    timer.style.width = bombLeftTime / bombStartTime * 100 + "%";
+  }, 16);
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-    createBombTimer();
-  });
+  createBombTimer();
+});
